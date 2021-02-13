d965474694607b133e14600cf266ab71f897e384
phan013 <truongvinh.phan@bertelsmann.de>
2021-02-13 20:03:30 +0100

    test graphql

--- apps/spike-frontend/pages/_app.tsx
+++ apps/spike-frontend/pages/_app.tsx
@@ -1,22 +1,32 @@
 import React from 'react';
+import {
+  QueryClient,
+  QueryClientProvider,
+} from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
 import { AppProps } from 'next/app';
 import Head from 'next/head';
 import { ReactComponent as NxLogo } from '../public/nx-logo-white.svg';
 import './styles.css';
 
+const queryClient = new QueryClient();
 function CustomApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>Welcome to spike-frontend!</title>
+        <title>Spike Frontend Test</title>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
       </Head>
       <div className="app">
         <header className="flex">
           <NxLogo width="75" height="50" />
-          <h1>Welcome to spike-frontend!</h1>
+          <h1>Spike Frontend</h1>
         </header>
         <main>
-          <Component {...pageProps} />
+          <QueryClientProvider client={queryClient}>
+            <Component {...pageProps} />
+            <ReactQueryDevtools initialIsOpen />
+          </QueryClientProvider>
         </main>
       </div>
     </>
--- apps/spike-frontend/pages/index.tsx
+++ apps/spike-frontend/pages/index.tsx
@@ -1,10 +1,126 @@
 import React from 'react';
 import styled from '@emotion/styled';
+import {
+  useQuery,
+  useQueryClient,
+  QueryClient,
+  QueryClientProvider,
+} from 'react-query';
+import { request, gql } from 'graphql-request';
+import { DataGrid, ColDef, ValueGetterParams, ValueFormatterParams } from '@material-ui/data-grid';
+
+const endpoint = 'https://one-as-spikebe-d01.azurewebsites.net/graphql';
 
 const StyledPage = styled.div`
   .page {
   }
 `;
+
+function useHumans() {
+  // return useQuery('humans', async () => {
+  //   const {
+  //     humans,
+  //   } = await request(
+  //     endpoint,
+  //     gql`
+  //       query {
+  //         humans {
+  //           name
+  //           appearsIn
+  //           id
+  //           homePlanet
+  //           friends {
+  //             name
+  //           }
+  //         }
+  //       }
+  //     `
+  //     );
+  //   return humans;
+  // });
+  return {
+    status: 'success',
+    data: [
+      {
+        name: 'Luke',
+        appearsIn: ['NEWHOPE', 'EMPIRE', 'JEDI'],
+        id: '1',
+        homePlanet: 'Tatooine',
+        friends: [
+          {
+            name: 'R2-D2',
+          },
+          {
+            name: 'C-3PO',
+          },
+        ],
+      },
+      {
+        name: 'Vader',
+        appearsIn: ['NEWHOPE', 'EMPIRE', 'JEDI'],
+        id: '2',
+        homePlanet: 'Tatooine',
+        friends: [],
+      },
+    ],
+    error: null,
+    isFetching: false
+  };
+}
+
+const Humans = () => {
+  const queryClient = useQueryClient();
+  const { status, data, error, isFetching } = useHumans();
+  const columns: ColDef[] = [
+    { field: 'id', headerName: 'ID', width: 70 },
+    { field: 'name', headerName: 'Name', width: 130 },
+    {
+      field: 'appearsIn', headerName: 'Appears in', width: 200, valueFormatter: (params: ValueFormatterParams) => {
+        return (params.value as string[]).join(', ');
+    } },
+    {
+      field: 'homePlanet',
+      headerName: 'Home planet',
+      width: 200,
+    },
+    {
+      field: 'friends',
+      headerName: 'Friends',
+      description: 'Friends of this character',
+      sortable: false,
+      width: 160,
+      valueFormatter: (params: ValueFormatterParams) => {
+        const vals = (params.value as any[]).map(friend => friend.name);
+        return vals.join(', ');
+      }
+    },
+  ];
+
+  return (
+    <div>
+      <h1>Humans</h1>
+      <div>
+        {status === 'loading' ? (
+          'Loading...'
+        ) : status === 'error' ? (
+          <span>Error: {(error as Error).message}</span>
+        ) : (
+          <>
+            <div style={{ height: 400, width: '100%' }}>
+              <DataGrid
+                rows={data}
+                columns={columns}
+                pageSize={5}
+                checkboxSelection
+              />
+            </div>
+            <div>{isFetching ? 'Background data refreshing...' : ' '}</div>
+          </>
+        )}
+      </div>
+    </div>
+  );
+};
 
 export function Index() {
   /*
@@ -14,88 +130,7 @@ export function Index() {
    */
   return (
     <StyledPage>
-      <h2>Resources &amp; Tools</h2>
-      <p>Thank you for using and showing some ♥ for Nx.</p>
-      <div className="flex github-star-container">
-        <a
-          href="https://github.com/nrwl/nx"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          {' '}
-          If you like Nx, please give it a star:
-          <div className="github-star-badge">
-            <img src="/star.svg" className="material-icons" alt="" />
-            Star
-          </div>
-        </a>
-      </div>
-      <p>Here are some links to help you get started.</p>
-      <ul className="resources">
-        <li className="col-span-2">
-          <a
-            className="resource flex"
-            href="https://egghead.io/playlists/scale-react-development-with-nx-4038"
-          >
-            Scale React Development with Nx (Course)
-          </a>
-        </li>
-        <li className="col-span-2">
-          <a
-            className="resource flex"
-            href="https://nx.dev/latest/react/tutorial/01-create-application"
-          >
-            Interactive tutorial
-          </a>
-        </li>
-        <li className="col-span-2">
-          <a className="resource flex" href="https://nx.app/">
-            <svg
-              width="36"
-              height="36"
-              viewBox="0 0 120 120"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M120 15V30C103.44 30 90 43.44 90 60C90 76.56 76.56 90 60 90C43.44 90 30 103.44 30 120H15C6.72 120 0 113.28 0 105V15C0 6.72 6.72 0 15 0H105C113.28 0 120 6.72 120 15Z"
-                fill="#0E2039"
-              />
-              <path
-                d="M120 30V105C120 113.28 113.28 120 105 120H30C30 103.44 43.44 90 60 90C76.56 90 90 76.56 90 60C90 43.44 103.44 30 120 30Z"
-                fill="white"
-              />
-            </svg>
-            <span className="gutter-left">Nx Cloud</span>
-          </a>
-        </li>
-      </ul>
-      <h2>Next Steps</h2>
-      <p>Here are some things you can do with Nx.</p>
-      <details open>
-        <summary>Add UI library</summary>
-        <pre>{`# Generate UI lib
-nx g @nrwl/react:lib ui
-
-# Add a component
-nx g @nrwl/react:component xyz --project ui`}</pre>
-      </details>
-      <details>
-        <summary>View dependency graph</summary>
-        <pre>{`nx dep-graph`}</pre>
-      </details>
-      <details>
-        <summary>Run affected commands</summary>
-        <pre>{`# see what's been affected by changes
-nx affected:dep-graph
-
-# run tests for current changes
-nx affected:test
-
-# run e2e tests for current changes
-nx affected:e2e
-`}</pre>
-      </details>
+      <Humans />
     </StyledPage>
   );
 }
